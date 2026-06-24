283dbff3d638f26e39a8bd83ed13e9fdf9b390c3
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2026-06-23 21:35:26 -0300

    Realizada a lógica da classe de suporte preparadorArquivos para trabalhar na parte 2 do projeto.

--- src/utils/preparadorArquivos.ts
+++ src/utils/preparadorArquivos.ts
@@ -37,8 +37,8 @@ export function lerArquivo(nomeArquivo: string): Entrada1 {
 //Lógica da parte 2
 
 export interface CustoTransporte {
-    cepOrigem: number;
-    cepDestino: number;
+    cepOrigem: string;
+    cepDestino: string;
     custo: number;
 }
 
@@ -47,4 +47,38 @@ export interface Entrada2 {
     ligacoes: CustoTransporte[];
     cepOrigem: string;
     cepDestino: string;
+}
+
+export function lerArquivo2(nomeArquivo: string): Entrada2{
+    const caminhoArquivo = path.resolve('arquivos', nomeArquivo);
+    const conteudoArquivo = fs.readFileSync(caminhoArquivo, 'utf-8');
+
+    const linhas = conteudoArquivo.trim().split('\n').map(l => l.trim());
+
+    const primSeparador = linhas.indexOf('--');
+    const segSeparador = linhas.indexOf('--', primSeparador + 1);
+
+    const cidades: Cidades[] = linhas.slice(0, primSeparador).map(linha => {
+        const [nome, cepInicio, cepFim] = linha.split(',');
+        return {
+            nome,
+            cepInicio: parseInt(cepInicio),
+            cepFim: parseInt(cepFim)
+        };
+    });
+
+    const ligacoes: CustoTransporte[] = linhas.slice(primSeparador + 1, segSeparador).map(linha => {
+        const [cepOrigem, cepDestino, custo] = linha.split(',');
+        return {
+            cepOrigem,
+            cepDestino,
+            custo: parseFloat(custo)
+        };
+    });
+
+    const ultimaLinha = linhas[segSeparador + 1];
+    const [cepOrigem, cepDestino] = ultimaLinha.split(',');
+
+    return {cidades, ligacoes, cepOrigem, cepDestino}
+
 }
